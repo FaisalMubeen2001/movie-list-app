a92ada9af9749f3dfd6fa4a3d09663b08510c4c2
FaisalMubeen2001 <faisalmubeen2001@gmail.com>
2025-08-26 14:03:43 +0530

    Initial commit - Movie List React App

---
+++ MovieList/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "c8f6905302cbe8236b47709a06ea5e00";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
